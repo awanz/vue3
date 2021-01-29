41f8a06b40b5ab0c31a02ddf250a88fbeae9d187
awanz <hermawan9815@gmail.com>
2021-01-29 17:47:25 +0700

    variable, if, for, vmodel, mounted, interval

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <dir class="app">
+    <p>Counter: {{ counter }}</p>
+    <p>{{ name }}</p>
+    <button v-on:click="awan">Generate</button>
+    <p>{{message}}</p>
+    <input v-model="message">
+    
+    <ol>
+      <li v-for="nl in namelists" :key="nl.message">
+        {{ nl }}
+      </li>
+    </ol>
+    <p v-if="seen">Now you see me</p>
+  </dir>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      counter: 3,
+      name: '',
+      message: '',
+      seen: false,
+      namelists: [
+        { text: 'awan'},
+        { text: 'bintang'},
+        { text: 'bulan'}
+      ]
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      if (this.counter % 2 == 0) {
+        this.seen = true
+      }else{
+        this.seen = false
+      }
+      this.counter++
+    }, 200)
+  },
+  methods: {
+    awan(){
+      this.name="awan"
+    }
+  },
 }
 </script>
 
